180ac3b46f89dd8120f9d51b99ca8bb773e9313a
Raymond Feng <raymond@strongloop.com>
2014-10-01 16:01:44 -0700

    Simplify the key gen

--- server/private/ssl-keygen.sh
+++ server/private/ssl-keygen.sh
@@ -6,15 +6,14 @@ KEY_LENGTH=2048
 # openssl x509 -req -in ca.csr -out ca.crt -signkey ca.key
 
 # Generate server key
-openssl genrsa -des3 -out server.key $KEY_LENGTH
-openssl req -new -key server.key -out server.csr
+openssl genrsa -passout pass:1234 -des3 -out server.key $KEY_LENGTH
+openssl req -passin pass:1234 -new -key server.key -out server.csr
 # Remove the passphrase
 cp server.key server.key.org
-openssl rsa -in server.key.org -out server.key
-rm server.key.org
+openssl rsa -passin pass:1234 -in server.key.org -out server.key
 
 # Generate server certificate
 openssl x509 -req -days 730 -in server.csr -signkey server.key -out server.pem
-rm server.csr
 cp server.pem certificate.pem 
 cp server.key privatekey.pem
+rm server.pem server.key server.key.org server.csr
