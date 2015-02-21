103210baea505d9a63f3a22d0c7de8d1244fe3b8
Raymond Feng <raymond@strongloop.com>
2015-02-21 13:01:47 -0800

    Add policy-based rate-limiting middleware

---
+++ server/middleware/rate-limiting-policy/index.js
@@ -0,0 +1,150 @@
+"use strict";
+var RateLimiter = require('./memory');
+var pf = require('loopback-policy');
+var Policy = pf.Policy;
+var Rule = pf.Rule;
+
+var debug = require('debug')('loopback:policy:rate-limiting');
+
+// First define object types for facts. There will be LoopBack models.
+function modelingFacts() {
+// Mock up the Context model
+  var Context = function(req, res) {
+    this.req = req;
+    this.res = res;
+    this.limits = {};
+  };
+
+// Mock up the Application model
+  var Application = function(id, name) {
+    this.id = id;
+    this.name = name;
+  };
+
+// Mock up the User model
+  var User = function(id, username, email) {
+    this.id = id;
+    this.username = username;
+    this.email = email;
+  };
+
+  return {
+    Context: Context,
+    Application: Application,
+    User: User
+  };
+}
+
+var models = modelingFacts();
+
+module.exports = function(options) {
+  options = options || {};
+  debug('Options: %j', options);
+
+// A global counters
+  var rateLimiter = new RateLimiter({
+    interval: options.interval || 1000,
+    limit: options.limit || 10});
+
+  function getHandler(session, key) {
+    var ctx = session.getFacts(models.Context)[0];
+    return function(err, result) {
+      if (ctx.proceed === false) {
+        return;
+      }
+      var res = ctx.res;
+      ctx.limits = ctx.limits || {};
+      ctx.limits[key] = result;
+      res.setHeader('X-RateLimit-Limit', result.limit);
+      res.setHeader('X-RateLimit-Remaining', result.remaining);
+      res.setHeader('X-RateLimit-Reset', result.reset);
+      ctx.proceed = result.isAllowed;
+      if (!result.isAllowed) {
+        res.status(429).json({error: 'Limit exceeded'});
+        session.halt();
+      }
+    };
+  }
+
+  var ruleForAppAndUser = new Rule("Limit requests based on application and user",
+    [
+      [models.Context, 'c', function(facts) {
+        var ctx = facts.c;
+        return ctx.proceed === undefined;
+      }],
+      [models.Application, 'a'],
+      [models.User, 'u', "u.username == 'bob'"]
+    ],
+    function(facts) {
+      debug('Action fired - Limit by app/user: %j %j', facts.a, facts.u);
+      var key = 'App-' + facts.a.id + '-User-' + facts.u.id;
+      rateLimiter.enforce(key, getHandler(this, key));
+    });
+
+  var ruleForIp = new Rule("Limit requests based on remote ip",
+    [
+      [models.Context, 'c', function(facts) {
+        var ctx = facts.c;
+        return ctx.proceed === undefined;
+      }],
+      [String, 'ip', "ip == '127.0.0.1' || ip == '::1'", "from c.req.ip"],
+    ], function(facts) {
+      debug('Action fired - Limit by ip: %s', facts.ip);
+      var key = 'IP-' + facts.ip;
+      rateLimiter.enforce(key, getHandler(this, key));
+    });
+
+  var policy = new Policy("Rate Limiting", [ruleForAppAndUser, ruleForIp]);
+
+  function buildFacts(ctx, cb) {
+    var facts = [];
+    var req = ctx.req;
+    if (req.accessToken) {
+      req.accessToken.user(function(err, u) {
+        if (err) {
+          return cb(err);
+        }
+        facts.push(new models.User(u.id, u.username, u.email));
+        req.accessToken.application(function(err, a) {
+          if (err) {
+            return cb(err);
+          }
+          facts.push(new models.Application(a.id, a.name));
+          cb(null, facts);
+        });
+      });
+    } else {
+      cb(null, facts);
+    }
+  }
+
+  return function enforceRateLimitingPolicy(req, res, next) {
+    var ctx = new models.Context(req, res);
+    buildFacts(ctx, function(err, facts) {
+      if (err) {
+        return next(err);
+      }
+      var session = policy.execute(ctx, facts, function(err) {
+        if (err) {
+          return next(err);
+        }
+        debug('Rule matching is done');
+        // session.print();
+        var ctx = session.getFacts(models.Context)[0];
+        if (ctx.limits) {
+          debug(ctx.limits);
+        }
+        if (ctx.proceed) {
+          next();
+        }
+      });
+    });
+  };
+
+};
+
+
+
+
+
+
---
+++ server/middleware/rate-limiting-policy/memory.js
@@ -0,0 +1,62 @@
+var debug = require('debug')('loopback:policy:rate-limiting');
+var limiter = require('limiter');
+
+module.exports = RateLimiter;
+
+function RateLimiter(options) {
+  if (!(this instanceof RateLimiter)) {
+    return new RateLimiter(options);
+  }
+  options = options || {};
+
+  this.limit = options.limit || 1000;
+  this.interval = options.interval || 1000; // ms
+  this.limiters = {};
+  this.options = options;
+}
+
+RateLimiter.prototype.getLimiter = function(key, limit) {
+  var inst;
+  debug('Key: %s', key);
+  if (key) {
+    inst = this.limiters[key];
+    if (!inst) {
+      debug('Creating rate limiter: %d %d', limit, this.interval);
+      inst = new limiter.RateLimiter(limit, this.interval);
+      this.limiters[key] = inst;
+    }
+  }
+  return inst;
+};
+
+RateLimiter.prototype.enforce = function(key, options, cb) {
+  if (cb === undefined && typeof options === 'function') {
+    cb = options;
+    options = {};
+  }
+  options = options || {};
+  var weight = options.weight || 1;
+  var limit = options.limit || this.limit;
+  var inst = this.getLimiter(key, limit);
+  if (inst) {
+    var ok = inst.tryRemoveTokens(weight);
+    debug('Bucket: ', inst.tokenBucket);
+    var remaining = Math.floor(inst.getTokensRemaining());
+    var reset = Math.max(this.interval - (Date.now() - inst.curIntervalStart), 0);
+
+    debug('Limit: %d Remaining: %d Reset: %d', limit, remaining, reset);
+
+    var result = {
+      limit: limit,
+      remaining: remaining,
+      reset: reset,
+      isAllowed: ok
+    };
+
+    process.nextTick(function() {
+      if (cb) {
+        cb(null, result);
+      }
+    });
+  }
+};
