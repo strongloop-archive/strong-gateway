84e7f2693b1a596b7ba730216527a89eed25e0bb
Simon Ho <simon@strongloop.com>
2015-05-27 15:02:49 -0700

    Fill out login form automatically

--- server/site.js
+++ server/site.js
@@ -3,7 +3,15 @@
  */
 
 exports.loginForm = function(req, res) {
-  res.render('login');
+  var demoUser;
+  if (process.env.NODE_ENV !== 'prod'
+      || process.env.NODE_ENV !== 'production') {
+    demoUser = {
+      username: 'bob',
+      password: 'secret'
+    };
+  }
+  res.render('login', {demoUser: demoUser});
 };
 
 exports.callbackPage = function(req, res) {
