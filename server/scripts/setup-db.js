e9e793b9100e4f4dfbf2baf9e020bf672d81b253
Raymond Feng <raymond@strongloop.com>
2015-06-09 15:17:10 -0700

    Fix the script to set up db

--- server/scripts/setup-db.js
+++ server/scripts/setup-db.js
@@ -1,3 +1,5 @@
+// Set the env to prod to avoid sample-data population
+process.env.NODE_ENV= 'prod';
 var app = require('../server');
 
 if (app.dataSources.db) {
@@ -7,5 +9,6 @@ if (app.dataSources.db) {
       console.error('Error in setting database: %j', err);
     }
     console.log('Database is set up');
+    app.dataSources.db.disconnect();
   });
 }
